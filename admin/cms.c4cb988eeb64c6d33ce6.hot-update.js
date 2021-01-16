89312da94a494ecdfc31655aa138e6628a6fc2f8
Tom Nguyen <tomtronwin@gmail.com>
2021-01-16 09:45:41 -0800

    Updates

---
+++ admin/cms.c4cb988eeb64c6d33ce6.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/components/all.sass":
+/*!*********************************!*\
+  !*** ./src/components/all.sass ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1610818848662
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.c4cb988eeb64c6d33ce6.hot-update.js.map
